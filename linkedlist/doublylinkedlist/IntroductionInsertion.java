892b8666a43ff5cb8be075068c376cfa6845a807
Aditya <adityadudhal97@gmail.com>
2025-01-04 18:33:04 +0530

    Doubly LinkedList

---
+++ linkedlist/doublylinkedlist/IntroductionInsertion.java
@@ -0,0 +1,22 @@
+package leetcode.linkedlist.doublylinkedlist;
+
+public class IntroductionInsertion {
+    Node constructDLL(int arr[]) {
+        Node root = null;
+        Node temp = null;
+        Node previousNode = null;
+        for (int element : arr) {
+            if (root == null) {
+                root = new Node(element);
+                temp = root;
+            } else {
+
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ linkedlist/doublylinkedlist/Node.java
@@ -0,0 +1,17 @@
+package leetcode.linkedlist.doublylinkedlist;
+
+public class Node {
+    int data;
+    Node prev;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        prev = null;
+        next = null;
+    }
+
+    public Node() {
+
+    }
+}
