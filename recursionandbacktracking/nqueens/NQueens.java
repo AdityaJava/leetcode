a978aa790b18077a61661da21ad2334f27d18400
AdityaDudhal <a.dudhal@maxxton.com>
2025-04-01 10:34:13 +0530

    N queens

--- recursionandbacktracking/nqueens/NQueens.java
+++ recursionandbacktracking/nqueens/NQueens.java
@@ -1,25 +1,37 @@
 package leetcode.recursionandbacktracking.nqueens;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * NQueens
  */
 public class NQueens {
 
-    private void nQueenRecursion(List<List<Character>> board, List<Integer> rowHash, List<Integer> lowerDiagonalHash, List<Integer> upperDiagonalHash, int column) {
+    private void nQueenRecursion(List<List<Character>> board, List<Integer> rowHash, List<Integer> lowerDiagonalHash, List<Integer> upperDiagonalHash, int column, List<List<String>> answers) {
+        if (column == board.size()) {
+            List<String> temp = new LinkedList<>();
+            for (int i = 0; i < board.size(); i++) {
+                temp.add(new String(board.get(i).stream().map(String::valueOf).collect(Collectors.joining())));
+            }
+            answers.add(new ArrayList<>(temp));
+            return;
+        }
 
         for (int row = 0; row < board.size(); row++) {
             if (isSafe(rowHash, lowerDiagonalHash, upperDiagonalHash, row, column, board.size())) {
                 rowHash.set(row, 1);
                 lowerDiagonalHash.set(row + column, 1);
-                upperDiagonalHash.set(board.size() - 1 + column + row, 1);
-                nQueenRecursion(board, rowHash, lowerDiagonalHash, upperDiagonalHash, column + 1);
+                upperDiagonalHash.set(board.size() - 1 + column - row, 1);
+                board.get(row).set(column, 'Q');
+                nQueenRecursion(board, rowHash, lowerDiagonalHash, upperDiagonalHash, column + 1, answers);
+                board.get(row).set(column, '.');
                 //Backtracking
                 rowHash.set(row, 0);
                 lowerDiagonalHash.set(row + column, 0);
-                upperDiagonalHash.set(board.size() - 1 + column + row, 0);
+                upperDiagonalHash.set(board.size() - 1 + column - row, 0);
             }
         }
 
@@ -34,14 +46,23 @@ public class NQueens {
         List<Integer> rowHash = new ArrayList<>();
         List<Integer> lowerDiagonalHash = new ArrayList<>();
         List<Integer> upperDiagonalHash = new ArrayList<>();
-        nQueenRecursion(board, rowHash, lowerDiagonalHash, upperDiagonalHash,0);
-        return null;
+        int size = board.size();
+        for (int i = 0; i < (2 * size-1); i++) {
+            rowHash.add(0);
+            lowerDiagonalHash.add(0);
+            upperDiagonalHash.add(0);
+        }
+
+        List<List<String>> answers = new LinkedList<>();
+        nQueenRecursion(board, rowHash, lowerDiagonalHash, upperDiagonalHash, 0, answers);
+        return answers;
     }
 
     private List<List<Character>> constructBoard(int n) {
         List<List<Character>> board = new ArrayList<>();
-        List<Character> columns = new ArrayList<>();
+        List<Character> columns;
         for (int i = 0; i < n; i++) {
+            columns = new ArrayList<>();
             for (int j = 0; j < n; j++) {
                 columns.add('.');
             }
@@ -51,6 +72,10 @@ public class NQueens {
     }
 
     public static void main(String[] args) {
-
+        int n = 4;
+        NQueens nQueens = new NQueens();
+        nQueens.solveNQueens(n).forEach(eachAnswer -> {
+            System.out.println(eachAnswer);
+        });
     }
 }
