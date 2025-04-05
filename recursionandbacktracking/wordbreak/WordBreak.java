582775b4b6b252b9403712c33fe8f3daaaac4470
AdityaDudhal <a.dudhal@maxxton.com>
2025-04-05 15:44:36 +0530

    Word brake

---
+++ recursionandbacktracking/wordbreak/WordBreak.java
@@ -0,0 +1,37 @@
+package leetcode.recursionandbacktracking.wordbreak;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * WordBreak
+ */
+public class WordBreak {
+    public static ArrayList<String> wordBreak(String s, ArrayList<String> dictionary) {
+        ArrayList<String> answers = new ArrayList<>();
+        recursiveWordBreak(s, 0, dictionary, answers, new String());
+        return answers;
+    }
+
+    private static void recursiveWordBreak(String s, int beginIndex, ArrayList<String> dictionary, List<String> answers, String answer) {
+        if (beginIndex == s.length()) {
+            answers.add(answer);
+            return;
+        }
+        String temp = new String();
+        for (int i = beginIndex; i < s.length(); i++) {
+            temp = s.substring(beginIndex, i + 1);
+            if (dictionary.contains(temp)) {
+                answer = temp + answer;
+                recursiveWordBreak(s, i + 1, dictionary, answers, answer);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "catsanddog";
+        ArrayList<String> dict = new ArrayList<>(Arrays.asList("cat", "cats", "and", "sand", "dog"));
+        WordBreak.wordBreak(s, dict).forEach(eachWord -> System.out.println(eachWord));
+    }
+}
