6ccb26093fa9dc5d7fe0617d31f2d6cc89bf2a05
AdityaDudhal <a.dudhal@maxxton.com>
2025-04-13 15:21:46 +0530

    Working correct

--- stackandqueue/next_greater_element/NextGreaterElement.java
+++ stackandqueue/next_greater_element/NextGreaterElement.java
@@ -1,5 +1,7 @@
 package leetcode.stackandqueue.next_greater_element;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 /**
@@ -8,31 +10,36 @@ import java.util.Stack;
 public class NextGreaterElement {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Stack<Integer> stack = new Stack<>();
-        int[] ans = new int[nums2.length];
-        int ansPtr = nums2.length - 1;
+        int[] nextGreater = new int[nums2.length];
+        int[] ans = new int[nums1.length];
+        Map<Integer, Integer> elementToNextGreater = new HashMap<>();
         for (int i = nums2.length - 1; i >= 0; i--) {
             if (stack.isEmpty()) {
-                ans[ansPtr--] = -1;
+                elementToNextGreater.put(nums2[i], -1);
                 stack.push(nums2[i]);
             }
             else if (nums2[i] < stack.peek()) {
-                ans[ansPtr--] = stack.peek();
+                elementToNextGreater.put(nums2[i], stack.peek());
                 stack.push(nums2[i]);
             }
             else if (nums2[i] >= stack.peek()) {
                 while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
                     stack.pop();
                 }
-                ans[ansPtr--] = stack.isEmpty() ? -1 : stack.peek();
+                elementToNextGreater.put(nums2[i], stack.isEmpty() ? -1 : stack.peek());
                 stack.push(nums2[i]);
             }
+        }
+        int i = 0;
+        for (int n : nums1) {
+            ans[i++] = elementToNextGreater.get(n);
         }
         return ans;
     }
 
     public static void main(String[] args) {
-        int[] nums2 = new int[] { 4, 12, 5, 3, 1, 2, 5, 3, 1, 2, 4, 6 };
-        int[] nums1 = new int[0];
+        int[] nums2 = new int[] { 1, 3, 4, 2 };
+        int[] nums1 = new int[] { 4, 1, 2 };
 
         NextGreaterElement nextGreaterElement = new NextGreaterElement();
         int[] ans = nextGreaterElement.nextGreaterElement(nums1, nums2);
